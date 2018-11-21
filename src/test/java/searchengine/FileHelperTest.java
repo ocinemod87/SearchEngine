9db7469ec0fce1c2d71a9fbc335acf9db557004e
Mikkel Smedemand <mbsm@itu.dk>
2018-11-21 16:46:59 +0100

    Modify method FileHelper.parseFile, to also check whether title or ListOfWords are null. Uncomment content in parseBadFile test.

--- src/test/java/searchengine/FileHelperTest.java
+++ src/test/java/searchengine/FileHelperTest.java
@@ -18,11 +18,11 @@ class FileHelperTest {
     @Test
     void parseBadFile() {
         List<Website> sites = FileHelper.parseFile("data/test-file-errors.txt");
-//        assertEquals(2, sites.size());
-//        assertEquals("title1", sites.get(0).getTitle());
-//        assertEquals("title2", sites.get(1).getTitle());
-//        assertTrue(sites.get(0).containsWord("word1"));
-//        assertFalse(sites.get(0).containsWord("word3"));
+        assertEquals(2, sites.size());
+        assertEquals("title1", sites.get(0).getTitle());
+        assertEquals("title2", sites.get(1).getTitle());
+        assertTrue(sites.get(0).containsWord("word1"));
+        assertFalse(sites.get(0).containsWord("word3"));
     }
 
 }
