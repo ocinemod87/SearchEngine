ee7d3cff91ce05a89863bb651771de8c9cca5384
Domenico Villani <domenicovill87@gmail.com>
2018-11-22 16:22:47 +0100

    I modified TreeMap and IndexMap classes as requested in the assignment

--- src/main/java/searchengine/InvertedIndexHashMap.java
+++ src/main/java/searchengine/InvertedIndexHashMap.java
@@ -1,8 +1,47 @@
 package searchengine;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class InvertedIndexHashMap extends InvertedIndex {
     public InvertedIndexHashMap() {
         this.map = new HashMap<>();
+    }
+
+    /**
+     *Takes a list of websites and creates a map. The keys are the words contained in these websites,
+     * the value is a list of all the websites containing that key word.
+     * @param sites The list of websites that should be indexed
+     */
+    @Override
+    public void build (List<Website> sites){
+
+        for(Website site: sites){
+            for(String word: site.getWords()){
+
+                //check if the map contains already an entry of this type
+                if(map.containsKey(word)){
+                    //if yes takes the value from this entry and add the website to it
+                    List<Website> websites = map.get(word);
+                    websites.add(site);
+                    map.put(word, websites);
+                }else{
+                    //if not create a new website list with the new website and add to the new entry
+                    List<Website> webTemp = new ArrayList<>();
+                    webTemp.add(site);
+                    map.put(word,webTemp);
+                }
+            }
+        }
+    }
+
+    /**
+     * Returns the List value mapped to the query String, returns null if the map does not contain the key
+     * @param query The query
+     * @return the List of website that contain the query word
+     */
+    @Override
+    public List<Website> lookup(String query){
+        return map.get(query);
     }
 }
--- src/main/java/searchengine/InvertedIndexTreeMap.java
+++ src/main/java/searchengine/InvertedIndexTreeMap.java
@@ -12,27 +12,39 @@ public class InvertedIndexTreeMap extends InvertedIndex {
     }
 
     /**
-     * 
+     *Takes a list of websites and creates a map. The keys are the words contained in these websites,
+     * the value is a list of all the websites containing that key word.
      * @param sites The list of websites that should be indexed
      */
+    @Override
     public void build (List<Website> sites){
 
-        for(Website w: sites){
-            for(String s: w.getWords()){
+        for(Website site: sites){
+            for(String word: site.getWords()){
 
                 //check if the map contains already an entry of this type
-                if(map.containsKey(s)){
+                if(map.containsKey(word)){
                     //if yes takes the value from this entry and add the website to it
-                    List<Website> websites = map.get(s);
-                    websites.add(w);
-                    map.put(s, websites);
+                    List<Website> websites = map.get(word);
+                    websites.add(site);
+                    map.put(word, websites);
                 }else{
                     //if not create a new website list with the new website and add to the new entry
                     List<Website> webTemp = new ArrayList<>();
-                    webTemp.add(w);
-                    map.put(s,webTemp);
+                    webTemp.add(site);
+                    map.put(word,webTemp);
                 }
             }
         }
+    }
+
+    /**
+     * Returns the List value mapped to the query String, returns null if the map does not contain the key
+     * @param query The query
+     * @return the List of website that contain the query word
+     */
+    @Override
+    public List<Website> lookup(String query){
+        return map.get(query);
     }
 }
