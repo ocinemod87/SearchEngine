21644aa683208d214b2610f0bd6f8926d872f792
André Kobæk <ak@kgstrandgade.dk>
2018-11-23 17:47:33 +0100

    Changed index to InvertedIndexTreeMap based on benchmark, reformated SearchEnigne to fit google code style

--- src/main/java/searchengine/SearchEngine.java
+++ src/main/java/searchengine/SearchEngine.java
@@ -4,39 +4,39 @@ import java.util.List;
 import java.util.ArrayList;
 
 /**
- * The search engine. Upon receiving a list of websites, it performs
- * the necessary configuration (i.e. building an index and a query
- * handler) to then be ready to receive search queries.
+ * The search engine. Upon receiving a list of websites, it performs the necessary configuration
+ * (i.e. building an index and a query handler) to then be ready to receive search queries.
  *
  * @author Willard Rafnsson
  * @author Martin Aumüller
  * @author Leonid Rusnac
  */
 public class SearchEngine {
-    private QueryHandler queryHandler;
 
-    /**
-     * Creates a {@code SearchEngine} object from a list of websites.
-     *
-     * @param sites the list of websites
-     */
-    public SearchEngine(List<Website> sites) {
-        Index idx = new InvertedIndexHashMap();
-        idx.build(sites);
-        queryHandler = new QueryHandler(idx);
-    }
+  private QueryHandler queryHandler;
 
-    /**
-     * Returns the list of websites matching the query.
-     *
-     * @param query the query
-     * @return the list of websites matching the query
-     */
-    public List<Website> search(String query) {
-        if (query == null || query.isEmpty() ) {
-            return new ArrayList<Website>();
-        }
-        List<Website> resultList = queryHandler.getMatchingWebsites(query);
-        return resultList;
+  /**
+   * Creates a {@code SearchEngine} object from a list of websites.
+   *
+   * @param sites the list of websites
+   */
+  public SearchEngine(List<Website> sites) {
+    Index idx = new InvertedIndexTreeMap();
+    idx.build(sites);
+    queryHandler = new QueryHandler(idx);
+  }
+
+  /**
+   * Returns the list of websites matching the query.
+   *
+   * @param query the query
+   * @return the list of websites matching the query
+   */
+  public List<Website> search(String query) {
+    if (query == null || query.isEmpty()) {
+      return new ArrayList<Website>();
     }
+    List<Website> resultList = queryHandler.getMatchingWebsites(query);
+    return resultList;
+  }
 }
