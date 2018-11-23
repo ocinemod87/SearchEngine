bbaee66b663cee67061527c2c2373f8631b4be27
Flemming Westberg <flso@itu.dk>
2018-11-23 17:40:30 +0100

    Updated InvertedIndex with correct styling, revised documentation, fixed null return value in lookup() and more concise mapping in build()

--- src/main/java/searchengine/InvertedIndex.java
+++ src/main/java/searchengine/InvertedIndex.java
@@ -1,52 +1,59 @@
 package searchengine;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
+/**
+ * The InvertedIndex data structure provides a way to build an index from a list of websites. It
+ * allows to lookup the websites that contain a query word. The InvertedIndex maps query words to
+ * websites allowing for a more effective lookup
+ */
 public abstract class InvertedIndex implements Index {
 
     protected Map<String, Collection<Website>> map;
+
     /**
-     *Takes a list of websites and creates a map. The keys are the words contained in these websites,
-     * the value is a list of all the websites containing that key word.
+     * Takes a list of websites and creates a map. The keys are the words contained in these
+     * websites, the value is a list of all the websites containing that key.
+     * 
      * @param sites The list of websites that should be indexed
      */
     @Override
-    public void build (Collection<Website> sites){
+    public void build(Collection<Website> sites) {
 
-        for(Website site : sites){
-            for(String word: site.getWords()){
+        for (Website site : sites) {
+            for (String word : site.getWords()) {
 
-                //check if the map contains already an entry of this type
-                if(map.containsKey(word)){
-                    //if yes takes the value from this entry and add the website to it
-                    Collection<Website> websites = map.get(word);
-                    websites.add(site);
-                    map.put(word, websites);
-                }else{
-                    //if not create a new website list with the new website and add to the new entry
+                // Check if the map contains the current key
+                if (map.containsKey(word)) {
+                    // If so, map the current site to this key
+                    map.get(word).add(site);
+                } else {
+                    // If not, create a new ArrayList at the position of the key
                     List<Website> webTemp = new ArrayList<>();
                     webTemp.add(site);
-                    map.put(word,webTemp);
+                    map.put(word, webTemp);
                 }
             }
         }
     }
 
     /**
-     * Returns the List value mapped to the query String, returns null if the map does not contain the key
-     * @param query The query
-     * @return the List of website that contain the query word
+     * Returns the collection of websites mapped to the query string, returns an empty
+     * {@code ArrayList} if the query string is not contained in the indexed map.
+     * 
+     * @param query the query string
+     * @return a {@code Collection<Website>} of websites that contain the query word
      */
     @Override
-    public Collection<Website> lookup(String query){
+    public Collection<Website> lookup(String query) {
 
-        if(map.containsKey(query)){
+        if (map.containsKey(query)) {
             return map.get(query);
         }
-        return null;
+        // Return an empty ArrayList
+        return new ArrayList<>();
     }
 }
