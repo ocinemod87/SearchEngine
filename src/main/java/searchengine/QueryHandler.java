0dd19a14d70852b8052871e9444d5f6bd77d2c0b
André Kobæk <ak@kgstrandgade.dk>
2018-12-05 16:25:35 +0100

    Fixed the indentation and a comment in queryhandler

--- src/main/java/searchengine/QueryHandler.java
+++ src/main/java/searchengine/QueryHandler.java
@@ -13,79 +13,80 @@ import java.util.regex.Pattern;
 
 public class QueryHandler {
 
-    /** The index the QueryHandler uses for answering queries. */
-    private Index idx = null;
+  /** The index the QueryHandler uses for answering queries. */
+  private Index idx = null;
 
-    /**
-     * The regex used to validate queries - and the corresponding {@code Pattern} and
-     * {@code Matcher} objects.
-     */
-    private final String REGEX = "\\b([-\\w]+)\\b";
-    private Pattern pattern;
-    private Matcher matcher;
+  /**
+   * The regex used to validate queries - and the corresponding {@code Pattern} and
+   * {@code Matcher} objects.
+   */
+  private final String REGEX = "\\b([-\\w]+)\\b";
+  private Pattern pattern;
+  private Matcher matcher;
 
-    /**
-     * The constructor
-     * 
-     * @param idx The index used by the QueryHandler.
-     */
-    public QueryHandler(Index idx) {
-        this.idx = idx;
-        pattern = Pattern.compile(REGEX);
-    }
+  /**
+   * The constructor
+   *
+   * @param idx The index used by the QueryHandler.
+   */
+  public QueryHandler(Index idx) {
+    this.idx = idx;
+    pattern = Pattern.compile(REGEX);
+  }
 
-    /**
-     * getMachingWebsites answers queries of the type "subquery1 OR subquery2 OR subquery3 ...". A
-     * "subquery" has the form "word1 word2 word3 ...". A website matches a subquery if all the
-     * words occur on the website. A website matches the whole query, if it matches at least one
-     * subquery.
-     *
-     * @param input the query string
-     * @return the set of websites that matches the query
-     */
-    public List<Website> getMatchingWebsites(String input) {
+  /**
+   * getMachingWebsites answers queries of the type "subquery1 OR subquery2 OR subquery3 ...". A
+   * "subquery" has the form "word1 word2 word3 ...". A website matches a subquery if all the
+   * words occur on the website. A website matches the whole query, if it matches at least one
+   * subquery.
+   *
+   * @param input the query string
+   * @return the set of websites that matches the query
+   */
+  public List<Website> getMatchingWebsites(String input) {
 
-        // Set for storing the combined results
-        Set<Website> results = new HashSet<>();
+    // Set for storing the combined results
+    Set<Website> results = new HashSet<>();
 
-        // The search query is split into sub queries by the keyword 'OR'
-        String[] subQueries = input.split("\\bOR\\b");
+    // The search query is split into sub queries by the keyword 'OR'
+    String[] subQueries = input.split("\\bOR\\b");
 
-        // Go through each of the sub queries and get the results
-        for (String query : subQueries) {
+    // Go through each of the sub queries and get the results
+    for (String query : subQueries) {
 
-            // Set for storing the results for this sub query
-            Set<Website> subResults = new HashSet<>();
+      // Set for storing the results for this sub query
+      Set<Website> subResults = new HashSet<>();
 
-            // Boolean to define whether the lookups should be added or retained
-            boolean firstQueryDone = false;
+      // Boolean to define whether the lookups should be added or retained
+      boolean firstQueryDone = false;
 
-            // If the query equals 'OR', there are no queries and the loop should be terminated
-            if (query.equals("OR")) {
-                break;
-            }            
+      // If the query consists of only 'OR' the split method returns 'OR',
+      // therefore there are no queries and the loop should be terminated
+      if (query.equals("OR")) {
+        break;
+      }
 
-            // The query string is converted to lowercase to match the case of the data
-            query = query.toLowerCase();
-            matcher = pattern.matcher(query);
+      // The query string is converted to lowercase to match the case of the data
+      query = query.toLowerCase();
+      matcher = pattern.matcher(query);
 
-            while (matcher.find()) {
-                if (!firstQueryDone) {
-                    subResults.addAll(idx.lookup(matcher.group()));
-                    firstQueryDone = true;
-                } else {
-                    subResults.retainAll(idx.lookup(matcher.group()));
-                }
-            }
-
-            results.addAll(subResults);
+      while (matcher.find()) {
+        if (!firstQueryDone) {
+          subResults.addAll(idx.lookup(matcher.group()));
+          firstQueryDone = true;
+        } else {
+          subResults.retainAll(idx.lookup(matcher.group()));
         }
+      }
 
-        // Simple conversion to a list (to avoid changing types throughout the application (right
-        // now (at least)))
-        List<Website> resultsAsList = new ArrayList<>();
-        resultsAsList.addAll(results);
-
-        return resultsAsList;
+      results.addAll(subResults);
     }
+
+    // Simple conversion to a list (to avoid changing types throughout the application (right
+    // now (at least)))
+    List<Website> resultsAsList = new ArrayList<>();
+    resultsAsList.addAll(results);
+
+    return resultsAsList;
+  }
 }
